6382a08d675703c12b80c5edfe72d4484ee71c11
OMJS2769 <78577852+OMJS2769@users.noreply.github.com>
2022-03-01 20:02:13 -0600

    hotfix

--- config/logging.php
+++ config/logging.php
@@ -49,7 +49,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel_' . date('Y-m-d') . '.log'),
+            'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 7,
         ],
